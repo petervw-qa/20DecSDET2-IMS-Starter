75a047c5c5faafde8a44f3b1b56e8420c19eecf8
Tukanami <67924806+Tukanami@users.noreply.github.com>
2021-01-21 16:09:22 +0000

    IMS-31 created Item DAO with verified CRUD functionality

---
+++ src/main/java/com/qa/ims/persistence/dao/ItemDao.java
@@ -0,0 +1,119 @@
+package com.qa.ims.persistence.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.qa.ims.persistence.domain.Item;
+import com.qa.ims.utils.DatabaseUtilities;
+
+public class ItemDao implements IDomainDao<Item> {
+	
+	public static final Logger LOGGER = LogManager.getLogger();
+
+	@Override
+	public List<Item> readAll() {
+        try (Connection connection = DatabaseUtilities.getInstance().getConnection();
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery("SELECT * FROM items");) {
+            List<Item> items = new ArrayList<>();
+            while (resultSet.next()) {
+                items.add(modelFromResultSet(resultSet));
+            }
+            return items;
+        } catch (SQLException e) {
+            LOGGER.debug(e);
+            LOGGER.error(e.getMessage());
+        }
+        return new ArrayList<>();
+	}
+
+	@Override
+	public Item create(Item item) {
+        try (Connection connection = DatabaseUtilities.getInstance().getConnection();
+                PreparedStatement statement = connection
+                        .prepareStatement("INSERT INTO items(name, value) VALUES (?, ?)");) {
+            statement.setString(1, item.getName());
+            statement.setDouble(2, item.getValue());
+            statement.executeUpdate();
+            return readLatest();
+        } catch (Exception e) {
+            LOGGER.debug(e);
+            LOGGER.error(e.getMessage());
+        }
+        return null;
+	}
+	
+	public Item read(Long id) {
+        try (Connection connection = DatabaseUtilities.getInstance().getConnection();
+                PreparedStatement statement = connection.prepareStatement("SELECT * FROM items WHERE id = ?");) {
+            statement.setLong(1, id);
+            ResultSet resultSet = statement.executeQuery();
+            resultSet.next();
+            return modelFromResultSet(resultSet);
+        } catch (Exception e) {
+            LOGGER.debug(e);
+            LOGGER.error(e.getMessage());
+        }
+        return null;
+    }
+
+	@Override
+	public Item update(Item item) {
+        try (Connection connection = DatabaseUtilities.getInstance().getConnection();
+                PreparedStatement statement = connection
+                        .prepareStatement("UPDATE customers SET name = ?, value = ? WHERE id = ?");) {
+            statement.setString(1, item.getName());
+            statement.setDouble(2, item.getValue());
+            statement.setLong(3, item.getId());
+            statement.executeUpdate();
+            return read(item.getId());
+        } catch (Exception e) {
+            LOGGER.debug(e);
+            LOGGER.error(e.getMessage());
+        }
+        return null;
+    }
+
+	@Override
+	public int delete(long id) {
+        try (Connection connection = DatabaseUtilities.getInstance().getConnection();
+                Statement statement = connection.createStatement();) {
+            return statement.executeUpdate("delete from items where id = " + id);
+        } catch (Exception e) {
+            LOGGER.debug(e);
+            LOGGER.error(e.getMessage());
+        }
+        return 0;
+    }
+
+	@Override
+	public Item modelFromResultSet(ResultSet resultSet) throws SQLException {
+        Long id = resultSet.getLong("id");
+        String name = resultSet.getString("name");
+        Double value = resultSet.getDouble("value");
+        return new Item(id, name, value);
+    }
+
+	
+	public Item readLatest() {
+        try (Connection connection = DatabaseUtilities.getInstance().getConnection();
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery("SELECT * FROM items ORDER BY id DESC LIMIT 1");) {
+            resultSet.next();
+            return modelFromResultSet(resultSet);
+        } catch (Exception e) {
+            LOGGER.debug(e);
+            LOGGER.error(e.getMessage());
+        }
+        return null;
+    }
+
+}
