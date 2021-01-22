8d8624c0e867bba49d01c5a77c3232976199cf9f
Tukanami <67924806+Tukanami@users.noreply.github.com>
2021-01-22 14:44:33 +0000

    IMS-17 added Delete functionality to Order Controller

--- src/main/java/com/qa/ims/controller/OrderController.java
+++ src/main/java/com/qa/ims/controller/OrderController.java
@@ -45,8 +45,8 @@ public class OrderController implements ICrudController<Order>  {
 
 	@Override
 	public int delete() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
+        LOGGER.info("Please enter the id of the order you would like to delete");
+        Long id = javaUtilities.getLong();
+        return orderDao.delete(id);
+    }
 }
