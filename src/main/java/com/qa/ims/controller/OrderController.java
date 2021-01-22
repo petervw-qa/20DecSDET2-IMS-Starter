895612ffef7d78d9e6aecced4fadc3e01e9b4ef6
Tukanami <67924806+Tukanami@users.noreply.github.com>
2021-01-22 14:39:46 +0000

    added Create functionality to Order Controller

--- src/main/java/com/qa/ims/controller/OrderController.java
+++ src/main/java/com/qa/ims/controller/OrderController.java
@@ -2,9 +2,19 @@ package com.qa.ims.controller;
 
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.qa.ims.persistence.dao.OrderDao;
 import com.qa.ims.persistence.domain.Order;
+import com.qa.ims.utils.JavaUtilities;
 
 public class OrderController implements ICrudController<Order>  {
+	
+	public static final Logger LOGGER = LogManager.getLogger();
+
+    private OrderDao orderDao;
+    private JavaUtilities javaUtilities;
 
 	@Override
 	public List<Order> readAll() {
@@ -14,9 +24,14 @@ public class OrderController implements ICrudController<Order>  {
 
 	@Override
 	public Order create() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+        LOGGER.info("Please enter the order ID");
+        Long id = javaUtilities.getLong();
+        LOGGER.info("Please enter a surname");
+        Long fk_customer_id = javaUtilities.getLong();
+        Order order = orderDao.create(new Order(id, fk_customer_id));
+        LOGGER.info("Customer created");
+        return order;
+    }
 
 	@Override
 	public Order update() {
