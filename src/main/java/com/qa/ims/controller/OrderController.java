f3ebd1b316c85de3bcc96aebf6198fcde212bb1b
Tukanami <67924806+Tukanami@users.noreply.github.com>
2021-01-22 14:32:16 +0000

    IMS-37 created Order Controller class in ims.controller package

---
+++ src/main/java/com/qa/ims/controller/OrderController.java
@@ -0,0 +1,33 @@
+package com.qa.ims.controller;
+
+import java.util.List;
+
+import com.qa.ims.persistence.domain.Order;
+
+public class OrderController implements ICrudController<Order>  {
+
+	@Override
+	public List<Order> readAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Order create() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Order update() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int delete() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
