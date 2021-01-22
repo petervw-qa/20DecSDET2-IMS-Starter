2223a6b269200308db9ed475135a8a95b6fe2e5c
Tukanami <67924806+Tukanami@users.noreply.github.com>
2021-01-22 15:07:39 +0000

    IMS-35 updated order doman with hashcode and equals override functionality

--- src/main/java/com/qa/ims/persistence/domain/Order.java
+++ src/main/java/com/qa/ims/persistence/domain/Order.java
@@ -29,6 +29,37 @@ public class Order {
 	public String toString() {
 		return "Order [id=" + id + ", fk_customer_id=" + fk_customer_id + "]";
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((fk_customer_id == null) ? 0 : fk_customer_id.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		if (fk_customer_id == null) {
+			if (other.fk_customer_id != null)
+				return false;
+		} else if (!fk_customer_id.equals(other.fk_customer_id))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 	
 	
 	
