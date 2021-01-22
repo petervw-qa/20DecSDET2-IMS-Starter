9776887f196bd99cc788972c63f46bb43f661d8f
Tukanami <67924806+Tukanami@users.noreply.github.com>
2021-01-22 11:37:46 +0000

    IMS-12 added Read functionality to Item Controller

--- src/main/java/com/qa/ims/controller/ItemController.java
+++ src/main/java/com/qa/ims/controller/ItemController.java
@@ -5,17 +5,27 @@ import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.qa.ims.persistence.dao.CustomerDao;
 import com.qa.ims.persistence.dao.ItemDao;
+import com.qa.ims.persistence.domain.Customer;
 import com.qa.ims.persistence.domain.Item;
 import com.qa.ims.utils.JavaUtilities;
 
 public class ItemController implements ICrudController<Item> {
+	
+	public static final Logger LOGGER = LogManager.getLogger();
+
+    private ItemDao itemDao;
+    private JavaUtilities javaUtilities;
 
 	@Override
 	public List<Item> readAll() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+        List<Item> items = itemDao.readAll();
+        for (Item item : items) {
+            LOGGER.info(item);
+        }
+        return items;
+    }
 
 	@Override
 	public Item create() {
