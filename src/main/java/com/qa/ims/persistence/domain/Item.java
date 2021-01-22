712eb36853098ee65a79e8d2f3775094987bb94d
Tukanami <67924806+Tukanami@users.noreply.github.com>
2021-01-22 15:03:10 +0000

    IMS-36 updated functionality with hashcode and equals override

--- src/main/java/com/qa/ims/persistence/domain/Item.java
+++ src/main/java/com/qa/ims/persistence/domain/Item.java
@@ -1,13 +1,13 @@
 package com.qa.ims.persistence.domain;
 
 public class Item {
-	
-	// variables 
+
+	// variables
 	private Long id;
 	private String name;
 	private double value;
-	
-	// constructors 
+
+	// constructors
 	public Item(String name, double value) {
 		super();
 		this.name = name;
@@ -50,9 +50,41 @@ public class Item {
 	public String toString() {
 		return "Item [id=" + id + ", name=" + name + ", value=" + value + "]";
 	}
-	
-	
-	
-	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(value);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Item other = (Item) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (Double.doubleToLongBits(value) != Double.doubleToLongBits(other.value))
+			return false;
+		return true;
+	}
 
 }
