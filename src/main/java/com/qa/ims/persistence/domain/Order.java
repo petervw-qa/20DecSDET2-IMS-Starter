ee200c1596f52abaf91438f466b1aebbfd0c1b58
Tukanami <67924806+Tukanami@users.noreply.github.com>
2021-01-25 14:22:41 +0000

    updated order class with new toString method and more variables for functionality

--- src/main/java/com/qa/ims/persistence/domain/Order.java
+++ src/main/java/com/qa/ims/persistence/domain/Order.java
@@ -1,16 +1,36 @@
 package com.qa.ims.persistence.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Order {
-	
+
 	private Long id;
-	private Long fk_customer_id;
-	
-	public Order(Long id, Long fk_customer_id) {
+	private Customer orderCustomer;
+	private Double orderValue;
+	private List<Item> orderItems = new ArrayList<>();
+
+	public Order(Customer orderCustomer, Double orderValue) {
+		super();
+		this.orderCustomer = orderCustomer;
+		this.orderValue = orderValue;
+	}
+
+	public Order(Long id, Customer orderCustomer, Double orderValue) {
 		super();
 		this.id = id;
-		this.fk_customer_id = fk_customer_id;
+		this.orderCustomer = orderCustomer;
+		this.orderValue = orderValue;
 	}
-	
+
+	public Order(Long id, Customer orderCustomer, Double orderValue, List<Item> orderItems) {
+		super();
+		this.id = id;
+		this.orderCustomer = orderCustomer;
+		this.orderValue = orderValue;
+		this.orderItems = orderItems;
+	}
+
 	public Long getId() {
 		return id;
 	}
@@ -18,24 +38,55 @@ public class Order {
 	public void setId(Long id) {
 		this.id = id;
 	}
-	public Long getFk_customer_id() {
-		return fk_customer_id;
+
+	public Customer getOrderCustomer() {
+		return orderCustomer;
 	}
-	public void setFk_customer_id(Long fk_customer_id) {
-		this.fk_customer_id = fk_customer_id;
+
+	public void setOrderCustomer(Customer orderCustomer) {
+		this.orderCustomer = orderCustomer;
 	}
-	
+
+	public Double getOrderValue() {
+		return orderValue;
+	}
+
+	public void setOrderValue(Double orderValue) {
+		this.orderValue = orderValue;
+	}
+
+	public List<Item> getOrderItems() {
+		return orderItems;
+	}
+
+	public void setOrderItems(List<Item> orderItems) {
+		this.orderItems = orderItems;
+	}
+
 	@Override
 	public String toString() {
-		return "Order [id=" + id + ", fk_customer_id=" + fk_customer_id + "]";
+		StringBuilder order = new StringBuilder();
+		order.append(String.format("%s: %s %s", this.id, orderCustomer.getFirstName(), orderCustomer.getSurname()));
+		if (this.orderItems.isEmpty()) {
+			order.append("\n -> No items have been found in this order.");
+		} else {
+			order.append(String.format(" - value = £%.2f", this.orderValue));
+			this.orderItems.forEach(item -> {
+				order.append("\n -> ");
+				order.append(String.format("%s: £%.2f", item.getName(), item.getValue()));
+			});
+		}
+		return order.toString();
 	}
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((fk_customer_id == null) ? 0 : fk_customer_id.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((orderCustomer == null) ? 0 : orderCustomer.hashCode());
+		result = prime * result + ((orderItems == null) ? 0 : orderItems.hashCode());
+		result = prime * result + ((orderValue == null) ? 0 : orderValue.hashCode());
 		return result;
 	}
 
@@ -48,20 +99,27 @@ public class Order {
 		if (getClass() != obj.getClass())
 			return false;
 		Order other = (Order) obj;
-		if (fk_customer_id == null) {
-			if (other.fk_customer_id != null)
-				return false;
-		} else if (!fk_customer_id.equals(other.fk_customer_id))
-			return false;
 		if (id == null) {
 			if (other.id != null)
 				return false;
 		} else if (!id.equals(other.id))
 			return false;
+		if (orderCustomer == null) {
+			if (other.orderCustomer != null)
+				return false;
+		} else if (!orderCustomer.equals(other.orderCustomer))
+			return false;
+		if (orderItems == null) {
+			if (other.orderItems != null)
+				return false;
+		} else if (!orderItems.equals(other.orderItems))
+			return false;
+		if (orderValue == null) {
+			if (other.orderValue != null)
+				return false;
+		} else if (!orderValue.equals(other.orderValue))
+			return false;
 		return true;
 	}
-	
-	
-	
 
 }
