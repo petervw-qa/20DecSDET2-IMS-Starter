c23cec282f83609c44cf12f2a103f419dd9a4ed8
Tukanami <67924806+Tukanami@users.noreply.github.com>
2021-01-26 14:56:30 +0000

    IMS-47 fixed SQL bugs

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -14,7 +14,7 @@ CREATE TABLE IF NOT EXISTS `ims`.`customers` (
 CREATE TABLE IF NOT EXISTS `ims` . `items` (
 	`id` INT(11) NOT NULL AUTO_INCREMENT,
 	`name` VARCHAR(50) NOT NULL,
-	`value` DECIMAL(5,2) NOT NULL,
+	`price` DECIMAL(5,2) NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
@@ -22,12 +22,12 @@ CREATE TABLE IF NOT EXISTS `ims` . `orders` (
 	`id` INT(11) NOT NULL AUTO_INCREMENT,
 	`fk_customers_id` INT NOT NULL,
 	PRIMARY KEY (`id`),
-	FOREIGN KEY (`fk_c_id`) REFERENCES customers(`id`)
+	FOREIGN KEY (`fk_customers_id`) REFERENCES customers(`id`)
 );
 
 CREATE TABLE IF NOT EXISTS `ims` . `orders_items` (
 	`fk_orders_id` INT NOT NULL,
 	`fk_items_id` INT NOT NULL,
-	FOREIGN KEY (`fk_o_id`) REFERENCES orders(`id`),
-	FOREIGN KEY (`fk_i_id`) REFERENCES items(`id`)
+	FOREIGN KEY (`fk_orders_id`) REFERENCES orders(`id`),
+	FOREIGN KEY (`fk_items_id`) REFERENCES items(`id`)
 );
