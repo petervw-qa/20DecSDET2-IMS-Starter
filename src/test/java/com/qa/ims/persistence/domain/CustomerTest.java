fff5b2b5dfbd850bd8f605987a344dc35532adf2
Tukanami <67924806+Tukanami@users.noreply.github.com>
2021-01-26 16:40:58 +0000

    IMS-48 testing the customer toString method

--- src/test/java/com/qa/ims/persistence/domain/CustomerTest.java
+++ src/test/java/com/qa/ims/persistence/domain/CustomerTest.java
@@ -1,10 +1,19 @@
 package com.qa.ims.persistence.domain;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 import nl.jqno.equalsverifier.EqualsVerifier;
 
 public class CustomerTest {
+	
+	@Test
+	public void testToString() {
+		Customer customer = new Customer(1L, "peter", "vaughan-williams");
+		String expected = "id:1 first name:peter surname:vaughan-williams";
+		assertEquals(expected, customer.toString());
+	}
 
 	@Test
 	public void testEquals() {
